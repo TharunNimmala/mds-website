39f45544b910629e32366545001830deb4cd8448
Tharun Kumar Nimmala <nimmalatarun436@gmail.com>
2021-05-04 00:04:15 -0700

    Added app web views

--- mds_website/urls.py
+++ mds_website/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from web.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main),
+    path('covidstats', stats),
 ]
--- web/views.py
+++ web/views.py
@@ -1,3 +1,17 @@
+import socket
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def main(request):
+    hostname = socket.gethostname()
+    return render(request, 'index.html', locals())
+
+
+def stats(request):
+    hostname = socket.gethostname()
+    return render(request, 'covidstats.html', locals())
